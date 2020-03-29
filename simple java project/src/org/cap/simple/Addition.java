8b2f588cb4ef095c3f0ff74bf6895dd1b8842288
rafi <rafi@r.mshome.net>
2020-03-29 18:34:05 +0530

    new method creation

--- simple java project/src/org/cap/simple/Addition.java
+++ simple java project/src/org/cap/simple/Addition.java
@@ -4,14 +4,23 @@ public class Addition {
 int a=10;
 int b=13;
 int c=a+b;
+int d=b-a;
+
 public void add()
 {
 	System.out.println(c);
+}
+public void sub()
+{
+	System.out.println(d);
 }
 public static void main(String[] args)
 {
 	Addition ad=new Addition();
 	ad.add();
+	Addition ad1=new Addition();
+	ad1.sub();
+	
 	//hello......
 	
 }
