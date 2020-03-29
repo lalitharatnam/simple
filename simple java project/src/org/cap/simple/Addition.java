00aa61132b784c607c682f05288917f94f34cf5c
rafi <rafi@r.mshome.net>
2020-03-29 15:14:44 +0530

    my first commit

---
+++ simple java project/src/org/cap/simple/Addition.java
@@ -0,0 +1,18 @@
+package org.cap.simple;
+
+public class Addition {
+int a=10;
+int b=13;
+int c=a+b;
+public void add()
+{
+	System.out.println(c);
+}
+public static void main(String[] args)
+{
+	Addition ad=new Addition();
+	ad.add();
+	
+	
+}
+}
